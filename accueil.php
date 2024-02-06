5f2c1fb4900d1d1193258519e1175a49c3f2a7d8
drakotona <dina.rakotonarivo@mmibordeaux.com>
2024-02-06 16:19:10 +0100

    Header + page accueil

---
+++ accueil.php
@@ -0,0 +1,52 @@
+<?php include "header.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOGART Accueil</title>
+    <!-- Bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+    <link rel="stylesheet" href="src/css/accueil.css" />
+    <link rel="stylesheet" href="src/css/reset.css" />
+</head>
+
+<body>
+    <!-- 1e page -->
+    <div class="container text-center titre">
+        <div class="row">
+            <div class="col">
+                <img src="/src/images/LogoMemoriaBlack.svg" alt="Logo" width="786" height="184"
+                    class="d-inline-block align-text-top">
+                <h1>LES EGLISES AUTREMENT</h1>
+            </div>
+            <div class="col">
+                <img src="/src/images/Vitrail.webp" alt="Image vitrail" width="482" height="667" class="img_vitrail">
+            </div>
+        </div>
+    </div>
+
+    <div class="container2">
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col">
+                <h2> MEMORIA C'EST QUOI ?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur. Leo cum vel neque <br>
+                    lectus. Ultrices felis neque feugiat tellus hendrerit <br>
+                    dolor ullamcorper cursus consequat. Venenatis diam hac sollicitudin <br>
+                    diam nec quis. Consectetur enim magna amet gravida eget vivamus semper metus.</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -4,21 +4,24 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Blog'Art</title>
-    <!-- Load CSS -->
-    <link rel="stylesheet" href="src/css/style.css" />
+
     <!-- Bootstrap CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+        <!-- Load CSS -->
+    <link rel="stylesheet" href="src/css/header.css"/>
+    <link rel="stylesheet" href="src/css/reset.css"/>
     <link rel="shortcut icon" type="image/x-icon" href="src/images/article.png" />
+
 </head>
 <?php
 //load config
 require_once 'config.php';
 ?>
 <body>
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg my-navbar ">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-    <img src="/src/images/LogoBlack.svg" alt="Logo" width="50" height="35" class="d-inline-block align-text-top">
+    <img src="/src/images/LogoBlack.svg" alt="Logo" width="55" height="40" class="d-inline-block align-text-top">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -32,17 +35,17 @@ require_once 'config.php';
           <a class="nav-link" href="/views/backend/dashboard.php">ARTICLES</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">ACCUEIL</a>
+          <a class="nav-link active" aria-current="page" href="/">CONTACT</a>
         </li>
       </ul>
     </div>
     <!--right align-->
     <div class="d-flex">
       
-      <a class="btn btn-primary m-1" href="/views/backend/security/login.php" role="button">Connexion</a>
-      <a class="btn btn-dark m-1" href="/views/backend/security/signup.php" role="button">Inscription</a>
+      <a class="btn m-1" href="/views/backend/security/login.php" role="button">Connexion</a>
+      <a class="btn m-1" href="/views/backend/security/signup.php" role="button">Inscription</a>
       <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Rechercher sur le site…" aria-label="Search" >
+      <input class="form-control me-2" type="search" placeholder="Recherche"  aria-label="Search" >
       </form>
     </div>
   </div>
