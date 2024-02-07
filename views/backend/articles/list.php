87549f7cfe73cbbf3222158424b5648c77baeea7
Lucas33860 <150592389+Lucas33860@users.noreply.github.com>
2024-02-07 07:53:28 +0100

    List article ok

--- views/backend/articles/list.php
+++ views/backend/articles/list.php
@@ -18,18 +18,9 @@ GROUP BY article.numArt;
                 <thead>
                     <tr>
                         <th>Id</th>
-                        <th>Articles</th>
-                        <th>Actions</th>
+                        <th>Date Création</th>
                         <th>Titre Article</th>
                         <th>Chapô</th>
-                        <th>Titre 1</th>
-                        <th>Paragraphe1</th>
-                        <th>Titre 2</th>
-                        <th>Paragraphe2</th>
-                        <th>Titre3</th>
-                        <th>Paragraphe3</th>
-                        <th>Conclusion</th>
-                        <th>URL Photo</th>
                         <th>Thématique</th>
                     </tr>
                 </thead>
@@ -38,17 +29,8 @@ GROUP BY article.numArt;
                         <tr>
                             <td><?php echo isset($article['numArt']) ? $article['numArt'] : ''; ?></td>
                             <td><?php echo isset($article['dtCreaArt']) ? $article['dtCreaArt'] : ''; ?></td>
-                            <td><?php echo isset($article['dtMajArt']) ? $article['dtMajArt'] : ''; ?></td>
                             <td><?php echo isset($article['libTitrArt']) ? $article['libTitrArt'] : ''; ?></td>
                             <td><?php echo isset($article['libChapoArt']) ? $article['libChapoArt'] : ''; ?></td>
-                            <td><?php echo isset($article['parag1Art']) ? $article['parag1Art'] : ''; ?></td>
-                            <td><?php echo isset($article['libSs1Titr1Art']) ? $article['libSs1Titr1Art'] : ''; ?></td>
-                            <td><?php echo isset($article['parag2Art']) ? $article['parag2Art'] : ''; ?></td>
-                            <td><?php echo isset($article['libSs2Titr2Art']) ? $article['libSs2Titr2Art'] : ''; ?></td>
-                            <td><?php echo isset($article['parag3Art']) ? $article['parag3Art'] : ''; ?></td>
-                            <td><?php echo isset($article['libSs3Titr3Art']) ? $article['libSs3Titr3Art'] : ''; ?></td>
-                            <td><?php echo isset($article['libConclArt']) ? $article['libConclArt'] : ''; ?></td>
-                            <td><?php echo isset($article['urlPhotArt']) ? $article['urlPhotArt'] : ''; ?></td>
                             <td><?php echo isset($article['libThem']) ? $article['libThem'] : ''; ?></td>
                        
                             <td>
